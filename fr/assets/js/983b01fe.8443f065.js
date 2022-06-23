413ce1f8d7ec9bc4d4f728efba477b8c942d0cbf
David Marquis <davidmarquis@me.com>
2022-06-22 20:58:30 -0400

    Deploy website - based on b3a9f059922c58600bbd852d2ce2cdaac3b948fa

---
+++ fr/assets/js/983b01fe.8443f065.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[3568],{4143:function(s){s.exports=JSON.parse('{"label":"product updates","permalink":"/fr/blog/tags/product-updates","allTagsPath":"/fr/blog/tags","count":1}')}}]);
